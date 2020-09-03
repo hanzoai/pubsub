wallyqs/nats-server

Better support for distinguishedNameMatch in TLS Auth
Signed-off-by: Waldemar Quevedo <wally@synadia.com>

internal/ldap/dn.go
@@ -1,0 +1,229 @@
+// Copyright (c) 2011-2015 Michael Mitton (mmitton@gmail.com)
+// Portions copyright (c) 2015-2016 go-ldap Authors
+package ldap
+
+import (
+	"bytes"
+	enchex "encoding/hex"
+	"errors"
+	"fmt"
+	"strings"
+	"crypto/x509/pkix"
+)
+
+var attributeTypeNames = map[string]string{
+	"2.5.4.6":  "C",
+	"2.5.4.10": "O",
+	"2.5.4.11": "OU",
+	"2.5.4.3":  "CN",
+	"2.5.4.5":  "SERIALNUMBER",
+	"2.5.4.7":  "L",
+	"2.5.4.8":  "ST",
+	"2.5.4.9":  "STREET",
+	"2.5.4.17": "POSTALCODE",
+	// TODO: Add others
+	"0.9.2342.19200300.100.1.25": "DC",	
+}
+
+// AttributeTypeAndValue represents an attributeTypeAndValue from https://tools.ietf.org/html/rfc4514
+type AttributeTypeAndValue struct {
+	// Type is the attribute type
+	Type string
+	// Value is the attribute value
+	Value string
+}
+
+// RelativeDN represents a relativeDistinguishedName from https://tools.ietf.org/html/rfc4514
+type RelativeDN struct {
+	Attributes []*AttributeTypeAndValue
+}
+
+// DN represents a distinguishedName from https://tools.ietf.org/html/rfc4514
+type DN struct {
+	RDNs []*RelativeDN
+}
+
+func FromCertSubject(subject pkix.Name) (*DN, error) {
+	dn := &DN{
+		RDNs: make([]*RelativeDN, 0),
+	}
+	for _, name := range subject.Names {
+		oidString := name.Type.String()
+		typeName, ok := attributeTypeNames[oidString]
+		if !ok {
+			return nil, fmt.Errorf("Invalid Type Name: %+v", name)
+		}
+		v, ok := name.Value.(string)
+		if !ok {
+			return nil, fmt.Errorf("Invalid Type Value: %+v", v)
+		}
+		rdn := &RelativeDN{
+			Attributes: []*AttributeTypeAndValue{
+				{
+					Type: typeName,
+					Value: v,
+				},
+			},
+		}
+		defer func () {
+			dn.RDNs = append(dn.RDNs, rdn)
+		}()
+	}
+	return dn, nil
+}
+
+// ParseDN returns a distinguishedName or an error.
+// The function respects https://tools.ietf.org/html/rfc4514
+func ParseDN(str string) (*DN, error) {
+	dn := new(DN)
+	dn.RDNs = make([]*RelativeDN, 0)
+	rdn := new(RelativeDN)
+	rdn.Attributes = make([]*AttributeTypeAndValue, 0)
+	buffer := bytes.Buffer{}
+	attribute := new(AttributeTypeAndValue)
+	escaping := false
+
+	unescapedTrailingSpaces := 0
+	stringFromBuffer := func() string {
+		s := buffer.String()
+		s = s[0 : len(s)-unescapedTrailingSpaces]
+		buffer.Reset()
+		unescapedTrailingSpaces = 0
+		return s
+	}
+
+	for i := 0; i < len(str); i++ {
+		char := str[i]
+		switch {
+		case escaping:
+			unescapedTrailingSpaces = 0
+			escaping = false
+			switch char {
+			case ' ', '"', '#', '+', ',', ';', '<', '=', '>', '\\':
+				buffer.WriteByte(char)
+				continue
+			}
+			// Not a special character, assume hex encoded octet
+			if len(str) == i+1 {
+				return nil, errors.New("got corrupted escaped character")
+			}
+
+			dst := []byte{0}
+			n, err := enchex.Decode([]byte(dst), []byte(str[i:i+2]))
+			if err != nil {
+				return nil, fmt.Errorf("failed to decode escaped character: %s", err)
+			} else if n != 1 {
+				return nil, fmt.Errorf("expected 1 byte when un-escaping, got %d", n)
+			}
+			buffer.WriteByte(dst[0])
+			i++
+		case char == '\\':
+			unescapedTrailingSpaces = 0
+			escaping = true
+		case char == '=':
+			attribute.Type = stringFromBuffer()
+			// FIXME: Certs using BER encoding not supported right now.
+		case char == ',' || char == '+':
+			// We're done with this RDN or value, push it
+			if len(attribute.Type) == 0 {
+				return nil, errors.New("incomplete type, value pair")
+			}
+			attribute.Value = stringFromBuffer()
+			rdn.Attributes = append(rdn.Attributes, attribute)
+			attribute = new(AttributeTypeAndValue)
+			if char == ',' {
+				dn.RDNs = append(dn.RDNs, rdn)
+				rdn = new(RelativeDN)
+				rdn.Attributes = make([]*AttributeTypeAndValue, 0)
+			}
+		case char == ' ' && buffer.Len() == 0:
+			// ignore unescaped leading spaces
+			continue
+		default:
+			if char == ' ' {
+				// Track unescaped spaces in case they are trailing and we need to remove them
+				unescapedTrailingSpaces++
+			} else {
+				// Reset if we see a non-space char
+				unescapedTrailingSpaces = 0
+			}
+			buffer.WriteByte(char)
+		}
+	}
+	if buffer.Len() > 0 {
+		if len(attribute.Type) == 0 {
+			return nil, errors.New("DN ended with incomplete type, value pair")
+		}
+		attribute.Value = stringFromBuffer()
+		rdn.Attributes = append(rdn.Attributes, attribute)
+		dn.RDNs = append(dn.RDNs, rdn)
+	}
+	return dn, nil
+}
+
+// Equal returns true if the DNs are equal as defined by rfc4517 4.2.15 (distinguishedNameMatch).
+// Returns true if they have the same number of relative distinguished names
+// and corresponding relative distinguished names (by position) are the same.
+func (d *DN) Equal(other *DN) bool {
+	if len(d.RDNs) != len(other.RDNs) {
+		return false
+	}
+	for i := range d.RDNs {
+		if !d.RDNs[i].Equal(other.RDNs[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+// AncestorOf returns true if the other DN consists of at least one RDN followed by all the RDNs of the current DN.
+// "ou=widgets,o=acme.com" is an ancestor of "ou=sprockets,ou=widgets,o=acme.com"
+// "ou=widgets,o=acme.com" is not an ancestor of "ou=sprockets,ou=widgets,o=foo.com"
+// "ou=widgets,o=acme.com" is not an ancestor of "ou=widgets,o=acme.com"
+func (d *DN) AncestorOf(other *DN) bool {
+	if len(d.RDNs) >= len(other.RDNs) {
+		return false
+	}
+	// Take the last `len(d.RDNs)` RDNs from the other DN to compare against
+	otherRDNs := other.RDNs[len(other.RDNs)-len(d.RDNs):]
+	for i := range d.RDNs {
+		if !d.RDNs[i].Equal(otherRDNs[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+// Equal returns true if the RelativeDNs are equal as defined by rfc4517 4.2.15 (distinguishedNameMatch).
+// Relative distinguished names are the same if and only if they have the same number of AttributeTypeAndValues
+// and each attribute of the first RDN is the same as the attribute of the second RDN with the same attribute type.
+// The order of attributes is not significant.
+// Case of attribute types is not significant.
+func (r *RelativeDN) Equal(other *RelativeDN) bool {
+	if len(r.Attributes) != len(other.Attributes) {
+		return false
+	}
+	return r.hasAllAttributes(other.Attributes) && other.hasAllAttributes(r.Attributes)
+}
+
+func (r *RelativeDN) hasAllAttributes(attrs []*AttributeTypeAndValue) bool {
+	for _, attr := range attrs {
+		found := false
+		for _, myattr := range r.Attributes {
+			if myattr.Equal(attr) {
+				found = true
+				break
+			}
+		}
+		if !found {
+			return false
+		}
+	}
+	return true
+}
+
+// Equal returns true if the AttributeTypeAndValue is equivalent to the specified AttributeTypeAndValue
+// Case of the attribute type is not significant
+func (a *AttributeTypeAndValue) Equal(other *AttributeTypeAndValue) bool {
+	return strings.EqualFold(a.Type, other.Type) && a.Value == other.Value
+}

server/auth.go
@@ -15,8 +15,6 @@ package server
 
 import (
 	"crypto/tls"
-	"crypto/x509/pkix"
-	"encoding/asn1"
 	"encoding/base64"
 	"fmt"
 	"net"
@@ -25,6 +23,7 @@ import (
 	"time"
 
 	"github.com/nats-io/jwt/v2"
+	"github.com/nats-io/nats-server/v2/internal/ldap"
 	"github.com/nats-io/nkeys"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -419,18 +418,39 @@ func (s *Server) processClientOrLeafAuthentication(c *client, opts *Options) boo
 			return false
 		}
 	} else if hasUsers {
-		// Check if we are tls verify and are mapping users from the client_certificate
+		// Check if we are tls verify and are mapping users from the client_certificate.
 		if tlsMap {
-			var euser string
-			authorized := checkClientTLSCertSubject(c, func(u string) bool {
-				var ok bool
-				user, ok = users[u]
-				if !ok {
-					c.Debugf("User in cert [%q], not found", u)
-					return false
+			authorized := checkClientTLSCertSubject(c, func(u string, certRDN *ldap.DN) (string, bool) {
+				// First do literal lookup using the resulting string representation
+				// of RDNSequence as implemented by the pkix package from Go.
+				if u != "" {
+					usr, ok := users[u]
+					if !ok {
+						return "", ok
+					}
+					user = usr
+					return usr.Username, ok
 				}
-				euser = u
-				return true
+
+				if certRDN == nil {
+					return "", false
+				}
+
+				// Look through the accounts for an RDN that is equal to the one
+				// presented by the certificate.
+				for _, usr := range users {
+					// TODO: Use this utility to make a full validation pass
+					// on start in case tlsmap feature is being used.
+					inputRDN, err := ldap.ParseDN(usr.Username)
+					if err != nil {
+						continue
+					}
+					if inputRDN.Equal(certRDN) {
+						user = usr
+						return usr.Username, true
+					}
+				}
+				return "", false
 			})
 			if !authorized {
 				s.mu.Unlock()
@@ -441,7 +461,7 @@ func (s *Server) processClientOrLeafAuthentication(c *client, opts *Options) boo
 			}
 			// Already checked that the client didn't send a user in connect
 			// but we set it here to be able to identify it in the logs.
-			c.opts.Username = euser
+			c.opts.Username = user.Username
 		} else {
 			if c.kind == CLIENT && c.opts.Username == "" && noAuthUser != "" {
 				if u, exists := users[noAuthUser]; exists {
@@ -567,7 +587,6 @@ func (s *Server) processClientOrLeafAuthentication(c *client, opts *Options) boo
 		}
 		return true
 	}
-
 	if user != nil {
 		ok = comparePasswords(user.Password, c.opts.Password)
 		// If we are authorized, register the user which will properly setup any permissions
@@ -596,31 +615,12 @@ func (s *Server) processClientOrLeafAuthentication(c *client, opts *Options) boo
 		// or the one specified in config (if provided).
 		return s.registerLeafWithAccount(c, opts.LeafNode.Account)
 	}
-
 	return false
 }
 
-func getTLSAuthDCs(rdns *pkix.RDNSequence) string {
-	dcOID := asn1.ObjectIdentifier{0, 9, 2342, 19200300, 100, 1, 25}
-	dcs := []string{}
-	for _, rdn := range *rdns {
-		if len(rdn) == 0 {
-			continue
-		}
-		for _, atv := range rdn {
-			value, ok := atv.Value.(string)
-			if !ok {
-				continue
-			}
-			if atv.Type.Equal(dcOID) {
-				dcs = append(dcs, "DC="+value)
-			}
-		}
-	}
-	return strings.Join(dcs, ",")
-}
+type tlsMapAuthFn func(string, *ldap.DN) (string, bool)
 
-func checkClientTLSCertSubject(c *client, fn func(string) bool) bool {
+func checkClientTLSCertSubject(c *client, fn tlsMapAuthFn) bool {
 	tlsState := c.GetTLSConnectionState()
 	if tlsState == nil {
 		c.Debugf("User required in cert, no TLS connection state")
@@ -647,48 +647,47 @@ func checkClientTLSCertSubject(c *client, fn func(string) bool) bool {
 	switch {
 	case hasEmailAddresses:
 		for _, u := range cert.EmailAddresses {
-			if fn(u) {
-				c.Debugf("Using email found in cert for auth [%q]", u)
+			if match, ok := fn(u, nil); ok {
+				c.Debugf("Using email found in cert for auth [%q]", match)
 				return true
 			}
 		}
 		fallthrough
 	case hasSANs:
 		for _, u := range cert.DNSNames {
-			if fn(u) {
-				c.Debugf("Using SAN found in cert for auth [%q]", u)
+			if match, ok := fn(u, nil); ok {
+				c.Debugf("Using SAN found in cert for auth [%q]", match)
 				return true
 			}
 		}
 	case hasURIs:
 		for _, u := range cert.URIs {
-			if fn(u.String()) {
-				c.Debugf("Using URI found in cert for auth [%q]", u)
+			if match, ok := fn(u.String(), nil); ok {
+				c.Debugf("Using URI found in cert for auth [%q]", match)
 				return true
 			}
 		}
 	}
 
-	// Try to get the full RDN Sequence that includes the domain components.
-	var rdns pkix.RDNSequence
-	if _, err := asn1.Unmarshal(cert.RawSubject, &rdns); err == nil {
-		// If found domain components then include roughly following
-		// the order from https://tools.ietf.org/html/rfc2253
-		rdn := cert.Subject.ToRDNSequence().String()
-		dcs := getTLSAuthDCs(&rdns)
-		if len(dcs) > 0 {
-			u := strings.Join([]string{rdn, dcs}, ",")
-			if fn(u) {
-				c.Debugf("Using RDNSequence for auth [%q]", u)
-				return true
-			}
+	// Try first to get the full RDN Sequence that includes the domain components in original order.
+	dn, err := ldap.FromCertSubject(cert.Subject)
+	if err == nil {
+		match, ok := fn("", dn)
+		if ok {
+			c.Debugf("Using DistinguishedNameMatch for auth [%q]", match)
+			return true
 		}
 	}
 
-	// Use the subject of the certificate.
-	u := cert.Subject.String()
-	c.Debugf("Using certificate subject for auth [%q]", u)
-	return fn(u)
+	// If no match, then use the string representation of the RDNSequence from the subject.
+	u := cert.Subject.ToRDNSequence().String()
+	if match, ok := fn(u, nil); ok {
+		c.Debugf("Using certificate subject for auth [%q]", match)
+		return true
+	}
+
+	c.Debugf("User in cert [%q], not found", u)
+	return false
 }
 
 // checkRouterAuth checks optional router authorization which can be nil or username/password.
@@ -707,8 +706,8 @@ func (s *Server) isRouterAuthorized(c *client) bool {
 	}
 
 	if opts.Cluster.TLSMap {
-		return checkClientTLSCertSubject(c, func(user string) bool {
-			return opts.Cluster.Username == user
+		return checkClientTLSCertSubject(c, func(user string, dn *ldap.DN) (string, bool) {
+			return "", opts.Cluster.Username == user
 		})
 	}
 
@@ -731,8 +730,8 @@ func (s *Server) isGatewayAuthorized(c *client) bool {
 
 	// Check whether TLS map is enabled, otherwise use single user/pass.
 	if opts.Gateway.TLSMap {
-		return checkClientTLSCertSubject(c, func(user string) bool {
-			return opts.Gateway.Username == user
+		return checkClientTLSCertSubject(c, func(user string, dn *ldap.DN) (string, bool) {
+			return "", opts.Gateway.Username == user
 		})
 	}
 
@@ -781,16 +780,15 @@ func (s *Server) isLeafNodeAuthorized(c *client) bool {
 	} else if len(opts.LeafNode.Users) > 0 {
 		if opts.LeafNode.TLSMap {
 			var user *User
-			found := checkClientTLSCertSubject(c, func(u string) bool {
+			found := checkClientTLSCertSubject(c, func(u string, dn *ldap.DN) (string, bool) {
 				// This is expected to be a very small array.
 				for _, usr := range opts.LeafNode.Users {
 					if u == usr.Username {
 						user = usr
-						return true
+						return u, true
 					}
 				}
-				c.Debugf("User in cert [%q], not found", u)
-				return false
+				return "", false
 			})
 			if !found {
 				return false

hanzoai/pubsub

internal/ldap/dn.go
@@ -1,0 +1,234 @@
+// Copyright (c) 2011-2015 Michael Mitton (mmitton@gmail.com)
+// Portions copyright (c) 2015-2016 go-ldap Authors
+package ldap
+
+import (
+	"bytes"
+	"crypto/x509/pkix"
+	enchex "encoding/hex"
+	"errors"
+	"fmt"
+	"strings"
+)
+
+var attributeTypeNames = map[string]string{
+	"2.5.4.6":  "C",
+	"2.5.4.10": "O",
+	"2.5.4.11": "OU",
+	"2.5.4.3":  "CN",
+	"2.5.4.5":  "SERIALNUMBER",
+	"2.5.4.7":  "L",
+	"2.5.4.8":  "ST",
+	"2.5.4.9":  "STREET",
+	"2.5.4.17": "POSTALCODE",
+	// FIXME: Add others.
+	"0.9.2342.19200300.100.1.25": "DC",
+}
+
+// AttributeTypeAndValue represents an attributeTypeAndValue from https://tools.ietf.org/html/rfc4514
+type AttributeTypeAndValue struct {
+	// Type is the attribute type
+	Type string
+	// Value is the attribute value
+	Value string
+}
+
+// RelativeDN represents a relativeDistinguishedName from https://tools.ietf.org/html/rfc4514
+type RelativeDN struct {
+	Attributes []*AttributeTypeAndValue
+}
+
+// DN represents a distinguishedName from https://tools.ietf.org/html/rfc4514
+type DN struct {
+	RDNs []*RelativeDN
+}
+
+// FromCertSubject takes a pkix.Name from a cert and returns a DN
+// that uses the same set.
+func FromCertSubject(subject pkix.Name) (*DN, error) {
+	dn := &DN{
+		RDNs: make([]*RelativeDN, 0),
+	}
+	for i := len(subject.Names) - 1; i >= 0; i-- {
+		name := subject.Names[i]
+		oidString := name.Type.String()
+		typeName, ok := attributeTypeNames[oidString]
+		if !ok {
+			return nil, fmt.Errorf("invalid type name: %+v", name)
+		}
+		v, ok := name.Value.(string)
+		if !ok {
+			return nil, fmt.Errorf("invalid type value: %+v", v)
+		}
+		rdn := &RelativeDN{
+			Attributes: []*AttributeTypeAndValue{
+				{
+					Type:  typeName,
+					Value: v,
+				},
+			},
+		}
+		dn.RDNs = append(dn.RDNs, rdn)
+	}
+	return dn, nil
+}
+
+// ParseDN returns a distinguishedName or an error.
+// The function respects https://tools.ietf.org/html/rfc4514
+func ParseDN(str string) (*DN, error) {
+	dn := new(DN)
+	dn.RDNs = make([]*RelativeDN, 0)
+	rdn := new(RelativeDN)
+	rdn.Attributes = make([]*AttributeTypeAndValue, 0)
+	buffer := bytes.Buffer{}
+	attribute := new(AttributeTypeAndValue)
+	escaping := false
+
+	unescapedTrailingSpaces := 0
+	stringFromBuffer := func() string {
+		s := buffer.String()
+		s = s[0 : len(s)-unescapedTrailingSpaces]
+		buffer.Reset()
+		unescapedTrailingSpaces = 0
+		return s
+	}
+
+	for i := 0; i < len(str); i++ {
+		char := str[i]
+		switch {
+		case escaping:
+			unescapedTrailingSpaces = 0
+			escaping = false
+			switch char {
+			case ' ', '"', '#', '+', ',', ';', '<', '=', '>', '\\':
+				buffer.WriteByte(char)
+				continue
+			}
+			// Not a special character, assume hex encoded octet
+			if len(str) == i+1 {
+				return nil, errors.New("got corrupted escaped character")
+			}
+
+			dst := []byte{0}
+			n, err := enchex.Decode([]byte(dst), []byte(str[i:i+2]))
+			if err != nil {
+				return nil, fmt.Errorf("failed to decode escaped character: %s", err)
+			} else if n != 1 {
+				return nil, fmt.Errorf("expected 1 byte when un-escaping, got %d", n)
+			}
+			buffer.WriteByte(dst[0])
+			i++
+		case char == '\\':
+			unescapedTrailingSpaces = 0
+			escaping = true
+		case char == '=':
+			attribute.Type = stringFromBuffer()
+			// Special case: If the first character in the value is # the following data
+			// is BER encoded. Throw an error since not supported right now.
+			if len(str) > i+1 && str[i+1] == '#' {
+				return nil, errors.New("unsupported BER encoding")
+			}
+		case char == ',' || char == '+':
+			// We're done with this RDN or value, push it
+			if len(attribute.Type) == 0 {
+				return nil, errors.New("incomplete type, value pair")
+			}
+			attribute.Value = stringFromBuffer()
+			rdn.Attributes = append(rdn.Attributes, attribute)
+			attribute = new(AttributeTypeAndValue)
+			if char == ',' {
+				dn.RDNs = append(dn.RDNs, rdn)
+				rdn = new(RelativeDN)
+				rdn.Attributes = make([]*AttributeTypeAndValue, 0)
+			}
+		case char == ' ' && buffer.Len() == 0:
+			// ignore unescaped leading spaces
+			continue
+		default:
+			if char == ' ' {
+				// Track unescaped spaces in case they are trailing and we need to remove them
+				unescapedTrailingSpaces++
+			} else {
+				// Reset if we see a non-space char
+				unescapedTrailingSpaces = 0
+			}
+			buffer.WriteByte(char)
+		}
+	}
+	if buffer.Len() > 0 {
+		if len(attribute.Type) == 0 {
+			return nil, errors.New("DN ended with incomplete type, value pair")
+		}
+		attribute.Value = stringFromBuffer()
+		rdn.Attributes = append(rdn.Attributes, attribute)
+		dn.RDNs = append(dn.RDNs, rdn)
+	}
+	return dn, nil
+}
+
+// Equal returns true if the DNs are equal as defined by rfc4517 4.2.15 (distinguishedNameMatch).
+// Returns true if they have the same number of relative distinguished names
+// and corresponding relative distinguished names (by position) are the same.
+func (d *DN) Equal(other *DN) bool {
+	if len(d.RDNs) != len(other.RDNs) {
+		return false
+	}
+	for i := range d.RDNs {
+		if !d.RDNs[i].Equal(other.RDNs[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+// AncestorOf returns true if the other DN consists of at least one RDN followed by all the RDNs of the current DN.
+// "ou=widgets,o=acme.com" is an ancestor of "ou=sprockets,ou=widgets,o=acme.com"
+// "ou=widgets,o=acme.com" is not an ancestor of "ou=sprockets,ou=widgets,o=foo.com"
+// "ou=widgets,o=acme.com" is not an ancestor of "ou=widgets,o=acme.com"
+func (d *DN) AncestorOf(other *DN) bool {
+	if len(d.RDNs) >= len(other.RDNs) {
+		return false
+	}
+	// Take the last `len(d.RDNs)` RDNs from the other DN to compare against
+	otherRDNs := other.RDNs[len(other.RDNs)-len(d.RDNs):]
+	for i := range d.RDNs {
+		if !d.RDNs[i].Equal(otherRDNs[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+// Equal returns true if the RelativeDNs are equal as defined by rfc4517 4.2.15 (distinguishedNameMatch).
+// Relative distinguished names are the same if and only if they have the same number of AttributeTypeAndValues
+// and each attribute of the first RDN is the same as the attribute of the second RDN with the same attribute type.
+// The order of attributes is not significant.
+// Case of attribute types is not significant.
+func (r *RelativeDN) Equal(other *RelativeDN) bool {
+	if len(r.Attributes) != len(other.Attributes) {
+		return false
+	}
+	return r.hasAllAttributes(other.Attributes) && other.hasAllAttributes(r.Attributes)
+}
+
+func (r *RelativeDN) hasAllAttributes(attrs []*AttributeTypeAndValue) bool {
+	for _, attr := range attrs {
+		found := false
+		for _, myattr := range r.Attributes {
+			if myattr.Equal(attr) {
+				found = true
+				break
+			}
+		}
+		if !found {
+			return false
+		}
+	}
+	return true
+}
+
+// Equal returns true if the AttributeTypeAndValue is equivalent to the specified AttributeTypeAndValue
+// Case of the attribute type is not significant
+func (a *AttributeTypeAndValue) Equal(other *AttributeTypeAndValue) bool {
+	return strings.EqualFold(a.Type, other.Type) && a.Value == other.Value
+}

server/auth.go
@@ -25,6 +25,7 @@ import (
 	"time"
 
 	"github.com/nats-io/jwt/v2"
+	"github.com/nats-io/nats-server/v2/internal/ldap"
 	"github.com/nats-io/nkeys"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -419,18 +420,39 @@ func (s *Server) processClientOrLeafAuthentication(c *client, opts *Options) boo
 			return false
 		}
 	} else if hasUsers {
-		// Check if we are tls verify and are mapping users from the client_certificate
+		// Check if we are tls verify and are mapping users from the client_certificate.
 		if tlsMap {
-			var euser string
-			authorized := checkClientTLSCertSubject(c, func(u string) bool {
-				var ok bool
-				user, ok = users[u]
-				if !ok {
-					c.Debugf("User in cert [%q], not found", u)
-					return false
+			authorized := checkClientTLSCertSubject(c, func(u string, certRDN *ldap.DN) (string, bool) {
+				// First do literal lookup using the resulting string representation
+				// of RDNSequence as implemented by the pkix package from Go.
+				if u != "" {
+					usr, ok := users[u]
+					if !ok {
+						return "", ok
+					}
+					user = usr
+					return usr.Username, ok
 				}
-				euser = u
-				return true
+
+				if certRDN == nil {
+					return "", false
+				}
+
+				// Look through the accounts for an RDN that is equal to the one
+				// presented by the certificate.
+				for _, usr := range users {
+					// TODO: Use this utility to make a full validation pass
+					// on start in case tlsmap feature is being used.
+					inputRDN, err := ldap.ParseDN(usr.Username)
+					if err != nil {
+						continue
+					}
+					if inputRDN.Equal(certRDN) {
+						user = usr
+						return usr.Username, true
+					}
+				}
+				return "", false
 			})
 			if !authorized {
 				s.mu.Unlock()
@@ -441,7 +463,7 @@ func (s *Server) processClientOrLeafAuthentication(c *client, opts *Options) boo
 			}
 			// Already checked that the client didn't send a user in connect
 			// but we set it here to be able to identify it in the logs.
-			c.opts.Username = euser
+			c.opts.Username = user.Username
 		} else {
 			if c.kind == CLIENT && c.opts.Username == "" && noAuthUser != "" {
 				if u, exists := users[noAuthUser]; exists {
@@ -567,7 +589,6 @@ func (s *Server) processClientOrLeafAuthentication(c *client, opts *Options) boo
 		}
 		return true
 	}
-
 	if user != nil {
 		ok = comparePasswords(user.Password, c.opts.Password)
 		// If we are authorized, register the user which will properly setup any permissions
@@ -596,7 +617,6 @@ func (s *Server) processClientOrLeafAuthentication(c *client, opts *Options) boo
 		// or the one specified in config (if provided).
 		return s.registerLeafWithAccount(c, opts.LeafNode.Account)
 	}
-
 	return false
 }
 
@@ -620,7 +640,9 @@ func getTLSAuthDCs(rdns *pkix.RDNSequence) string {
 	return strings.Join(dcs, ",")
 }
 
-func checkClientTLSCertSubject(c *client, fn func(string) bool) bool {
+type tlsMapAuthFn func(string, *ldap.DN) (string, bool)
+
+func checkClientTLSCertSubject(c *client, fn tlsMapAuthFn) bool {
 	tlsState := c.GetTLSConnectionState()
 	if tlsState == nil {
 		c.Debugf("User required in cert, no TLS connection state")
@@ -647,48 +669,71 @@ func checkClientTLSCertSubject(c *client, fn func(string) bool) bool {
 	switch {
 	case hasEmailAddresses:
 		for _, u := range cert.EmailAddresses {
-			if fn(u) {
-				c.Debugf("Using email found in cert for auth [%q]", u)
+			if match, ok := fn(u, nil); ok {
+				c.Debugf("Using email found in cert for auth [%q]", match)
 				return true
 			}
 		}
 		fallthrough
 	case hasSANs:
 		for _, u := range cert.DNSNames {
-			if fn(u) {
-				c.Debugf("Using SAN found in cert for auth [%q]", u)
+			if match, ok := fn(u, nil); ok {
+				c.Debugf("Using SAN found in cert for auth [%q]", match)
 				return true
 			}
 		}
 	case hasURIs:
 		for _, u := range cert.URIs {
-			if fn(u.String()) {
-				c.Debugf("Using URI found in cert for auth [%q]", u)
+			if match, ok := fn(u.String(), nil); ok {
+				c.Debugf("Using URI found in cert for auth [%q]", match)
 				return true
 			}
 		}
 	}
 
-	// Try to get the full RDN Sequence that includes the domain components.
+	// Use the string representation of the full RDN Sequence including
+	// the domain components in case there are any.
+	rdn := cert.Subject.ToRDNSequence().String()
+
+	// Match that follows original order from the subject takes precedence.
+	dn, err := ldap.FromCertSubject(cert.Subject)
+	if err == nil {
+		if match, ok := fn("", dn); ok {
+			c.Debugf("Using DistinguishedNameMatch for auth [%q]", match)
+			return true
+		}
+		c.Debugf("DistinguishedNameMatch could not be used for auth [%q]", rdn)
+	}
+
 	var rdns pkix.RDNSequence
 	if _, err := asn1.Unmarshal(cert.RawSubject, &rdns); err == nil {
 		// If found domain components then include roughly following
 		// the order from https://tools.ietf.org/html/rfc2253
-		rdn := cert.Subject.ToRDNSequence().String()
+		//
+		// NOTE: The original sequence from string representation by ToRDNSequence does not follow
+		// the correct ordering, so this addition ofdomainComponents would likely be deprecated in
+		// another release in favor of using the correct ordered as parsed by the go-ldap library.
+		//
 		dcs := getTLSAuthDCs(&rdns)
 		if len(dcs) > 0 {
 			u := strings.Join([]string{rdn, dcs}, ",")
-			if fn(u) {
-				c.Debugf("Using RDNSequence for auth [%q]", u)
+			if match, ok := fn(u, nil); ok {
+				c.Debugf("Using RDNSequence for auth [%q]", match)
 				return true
 			}
+			c.Debugf("RDNSequence could not be used for auth [%q]", u)
 		}
 	}
 
-	// Use the subject of the certificate.
-	u := cert.Subject.String()
-	c.Debugf("Using certificate subject for auth [%q]", u)
-	return fn(u)
+	// If no match, then use the string representation of the RDNSequence
+	// from the subject without the domainComponents.
+	if match, ok := fn(rdn, nil); ok {
+		c.Debugf("Using certificate subject for auth [%q]", match)
+		return true
+	}
+
+	c.Debugf("User in cert [%q], not found", rdn)
+	return false
 }
 
 // checkRouterAuth checks optional router authorization which can be nil or username/password.
@@ -707,8 +752,8 @@ func (s *Server) isRouterAuthorized(c *client) bool {
 	}
 
 	if opts.Cluster.TLSMap {
-		return checkClientTLSCertSubject(c, func(user string) bool {
-			return opts.Cluster.Username == user
+		return checkClientTLSCertSubject(c, func(user string, _ *ldap.DN) (string, bool) {
+			return "", opts.Cluster.Username == user
 		})
 	}
 
@@ -731,8 +776,8 @@ func (s *Server) isGatewayAuthorized(c *client) bool {
 
 	// Check whether TLS map is enabled, otherwise use single user/pass.
 	if opts.Gateway.TLSMap {
-		return checkClientTLSCertSubject(c, func(user string) bool {
-			return opts.Gateway.Username == user
+		return checkClientTLSCertSubject(c, func(user string, _ *ldap.DN) (string, bool) {
+			return "", opts.Gateway.Username == user
 		})
 	}
 
@@ -781,16 +826,15 @@ func (s *Server) isLeafNodeAuthorized(c *client) bool {
 	} else if len(opts.LeafNode.Users) > 0 {
 		if opts.LeafNode.TLSMap {
 			var user *User
-			found := checkClientTLSCertSubject(c, func(u string) bool {
+			found := checkClientTLSCertSubject(c, func(u string, _ *ldap.DN) (string, bool) {
 				// This is expected to be a very small array.
 				for _, usr := range opts.LeafNode.Users {
 					if u == usr.Username {
 						user = usr
-						return true
+						return u, true
 					}
 				}
-				c.Debugf("User in cert [%q], not found", u)
-				return false
+				return "", false
 			})
 			if !found {
 				return false