nats-io/nats-server

(2.12) [FIXED] Isolated DirectGet reads & consistent pending count
Signed-off-by: Maurice van Veen <github@mauricevanveen.com>

server/jetstream_api.go
@@ -3453,6 +3453,10 @@ func (s *Server) jsMsgGetRequest(sub *subscription, c *client, _ *Account, subje
 	var svp StoreMsg
 	var sm *StoreMsg
 
+	// Ensure this read request is isolated and doesn't interleave with writes.
+	mset.isolateMu.RLock()
+	defer mset.isolateMu.RUnlock()
+
 	// If AsOfTime is set, perform this first to get the sequence.
 	var seq uint64
 	if req.StartTime != nil {

server/jetstream_batching_test.go
@@ -23,6 +23,7 @@ import (
 	"math/big"
 	"strconv"
 	"strings"
+	"sync"
 	"testing"
 	"time"
 
@@ -1502,4 +1503,129 @@ func TestJetStreamAtomicBatchPublishContinuousBatchesStillMoveAppliedUp(t *testi
 	checkFor(t, 2*time.Second, 200*time.Millisecond, func() error {
 		return checkState(t, c, globalAccountName, "TEST")
 	})
+}
+
+func TestJetStreamRollupIsolatedRead(t *testing.T) {
+	const (
+		DirectBatchGet = iota
+		DirectMultiGet
+		Consumer
+	)
+
+	test := func(t *testing.T, mode int) {
+		c := createJetStreamClusterExplicit(t, "R3S", 3)
+		defer c.shutdown()
+
+		nc, js := jsClientConnect(t, c.randomServer())
+		defer nc.Close()
+
+		_, err := js.AddStream(&nats.StreamConfig{
+			Name:        "TEST",
+			Subjects:    []string{"foo.*"},
+			AllowRollup: true,
+			Replicas:    3,
+		})
+		require_NoError(t, err)
+
+		rs := c.randomNonStreamLeader(globalAccountName, "TEST")
+		mset, err := rs.globalAccount().lookupStream("TEST")
+		require_NoError(t, err)
+
+		// Reconnect to the selected server.
+		nc.Close()
+		nc, js = jsClientConnect(t, rs)
+		defer nc.Close()
+
+		m := nats.NewMsg("foo.0")
+		pubAck, err := js.PublishMsg(m)
+		require_NoError(t, err)
+		require_Equal(t, pubAck.Sequence, 1)
+
+		// mset.processJetStreamMsg will first store the new message, then update dedupe state, and then do the rollup.
+		// We block the replica such that it can store the new message but can't do the rollup yet.
+		// A read should wait for the rollup to complete.
+		mset.ddMu.Lock()
+		m.Subject = "foo.1"
+		m.Header.Set(JSMsgRollup, JSMsgRollupAll)
+		m.Header.Set(JSMsgId, "msgId")
+		_, _ = js.PublishMsg(m)
+		err = checkForErr(2*time.Second, 200*time.Millisecond, func() error {
+			var state StreamState
+			mset.store.FastState(&state)
+			if state.LastSeq != 2 {
+				return fmt.Errorf("expected last seq 2, got: %d", state.LastSeq)
+			}
+			return nil
+		})
+		if err != nil {
+			mset.ddMu.Unlock()
+			require_NoError(t, err)
+		}
+
+		// We're now subscribing and going to do a request, while the write is still halfway.
+		sub, err := nc.SubscribeSync("reply")
+		if err != nil {
+			mset.ddMu.Unlock()
+			require_NoError(t, err)
+		}
+		defer sub.Drain()
+		if err = nc.Flush(); err != nil {
+			mset.ddMu.Unlock()
+			require_NoError(t, err)
+		}
+
+		// Run two goroutines, one will unblock the first write after a short sleep,
+		// the second will do the read/consumer request.
+		var (
+			ready   sync.WaitGroup
+			run     sync.WaitGroup
+			cleanup sync.WaitGroup
+		)
+		ready.Add(2)
+		run.Add(1)
+		cleanup.Add(1)
+		go func() {
+			ready.Done()
+			defer cleanup.Done()
+			run.Wait()
+			time.Sleep(250 * time.Millisecond)
+			mset.ddMu.Unlock()
+		}()
+		go func() {
+			ready.Done()
+			run.Wait()
+			switch mode {
+			case DirectBatchGet:
+				mset.getDirectRequest(&JSApiMsgGetRequest{NextFor: "foo.*", Batch: 2}, "reply")
+			case DirectMultiGet:
+				mset.getDirectRequest(&JSApiMsgGetRequest{MultiLastFor: []string{"foo.*"}, Batch: 2}, "reply")
+			case Consumer:
+				mset.addConsumer(&ConsumerConfig{DeliverSubject: "reply", Direct: true})
+			}
+		}()
+
+		// Wait for both to be ready, then run both of them.
+		ready.Wait()
+		run.Done()
+
+		msg, err := sub.NextMsg(time.Second)
+		// Make sure cleanup has happened before validating.
+		cleanup.Wait()
+		require_NoError(t, err)
+		if mode != Consumer {
+			require_Equal(t, msg.Header.Get(JSNumPending), "0")
+			require_Equal(t, msg.Header.Get(JSSequence), "2")
+		} else {
+			// $JS.ACK.<stream>.<consumer>.<delivered>.<sseq>.<cseq>.<tm>.<pending>
+			require_True(t, strings.HasPrefix(msg.Reply, jsAckPre))
+			tokens := strings.Split(msg.Reply, ".")
+			require_Len(t, len(tokens), 9)
+			require_Equal(t, tokens[8], "0") // pending
+			require_Equal(t, tokens[5], "2") // sseq
+		}
+	}
+
+	t.Run("BatchGet", func(t *testing.T) { test(t, DirectBatchGet) })
+	t.Run("MultiGet", func(t *testing.T) { test(t, DirectMultiGet) })
+	t.Run("Consumer", func(t *testing.T) { test(t, Consumer) })
 }

server/stream.go
@@ -327,6 +327,8 @@ type stream struct {
 	client *client      // The internal JetStream client.
 	sysc   *client      // The internal JetStream system client.
 
+	isolateMu sync.RWMutex // Read/write isolation lock for the stream. Used to guarantee atomic reads.
+
 	// The current last subscription ID for the subscriptions through `client`.
 	// Those subscriptions are for the subjects filters being listened to and captured by the stream.
 	sid atomic.Uint64
@@ -4730,11 +4732,14 @@ func (mset *stream) getDirectMulti(req *JSApiMsgGetRequest, reply string) {
 	// TODO(dlc) - Make configurable?
 	const maxAllowedResponses = 1024
 
-	// We hold the lock here to try to avoid changes out from underneath of us.
-	mset.mu.RLock()
-	defer mset.mu.RUnlock()
+	// We hold the lock here to avoid changes out from underneath of us.
+	mset.isolateMu.RLock()
+	defer mset.isolateMu.RUnlock()
+
 	// Grab store and name.
+	mset.mu.RLock()
 	store, name, s := mset.store, mset.cfg.Name, mset.srv
+	mset.mu.RUnlock()
 
 	// Grab MaxBytes
 	mb := req.MaxBytes
@@ -4782,7 +4787,7 @@ func (mset *stream) getDirectMulti(req *JSApiMsgGetRequest, reply string) {
 		return
 	}
 
-	np, lseq, sentBytes, sent := uint64(len(seqs)-1), uint64(0), 0, 0
+	np, lseq, sentBytes, sent := uint64(len(seqs)), uint64(0), 0, 0
 	for _, seq := range seqs {
 		if seq < req.Seq {
 			if np > 0 {
@@ -4801,6 +4806,10 @@ func (mset *stream) getDirectMulti(req *JSApiMsgGetRequest, reply string) {
 		hdr := sm.hdr
 		ts := time.Unix(0, sm.ts).UTC()
 
+		// Decrement num pending. This is optimization and we do not continue to look it up for these operations.
+		if np > 0 {
+			np--
+		}
 		if len(hdr) == 0 {
 			hdr = fmt.Appendf(nil, dgb, name, sm.subj, sm.seq, ts.Format(time.RFC3339Nano), np, lseq)
 		} else {
@@ -4811,10 +4820,6 @@ func (mset *stream) getDirectMulti(req *JSApiMsgGetRequest, reply string) {
 			hdr = genHeader(hdr, JSTimeStamp, ts.Format(time.RFC3339Nano))
 			hdr = genHeader(hdr, JSNumPending, strconv.FormatUint(np, 10))
 			hdr = genHeader(hdr, JSLastSequence, strconv.FormatUint(lseq, 10))
-		}
-		// Decrement num pending. This is optimization and we do not continue to look it up for these operations.
-		if np > 0 {
-			np--
 		}
 		// Track our lseq
 		lseq = sm.seq
@@ -4848,6 +4853,10 @@ func (mset *stream) getDirectRequest(req *JSApiMsgGetRequest, reply string) {
 	mset.mu.RLock()
 	store, name, s := mset.store, mset.cfg.Name, mset.srv
 	mset.mu.RUnlock()
+
+	// Ensure this read request is isolated and doesn't interleave with writes.
+	mset.isolateMu.RLock()
+	defer mset.isolateMu.RUnlock()
 
 	var seq uint64
 	// Lookup start seq if AsOfTime is set.
@@ -4918,6 +4927,10 @@ func (mset *stream) getDirectRequest(req *JSApiMsgGetRequest, reply string) {
 		if !req.NoHeaders {
 			hdr = sm.hdr
 			if isBatchRequest {
+				// Decrement num pending. This is optimization and we do not continue to look it up for these operations.
+				if np > 0 {
+					np--
+				}
 				if len(hdr) == 0 {
 					hdr = fmt.Appendf(nil, dgb, name, sm.subj, sm.seq, ts.Format(time.RFC3339Nano), np, lseq)
 				} else {
@@ -4929,8 +4942,6 @@ func (mset *stream) getDirectRequest(req *JSApiMsgGetRequest, reply string) {
 					hdr = genHeader(hdr, JSNumPending, strconv.FormatUint(np, 10))
 					hdr = genHeader(hdr, JSLastSequence, strconv.FormatUint(lseq, 10))
 				}
-				// Decrement num pending. This is optimization and we do not continue to look it up for these operations.
-				np--
 			} else {
 				if len(hdr) == 0 {
 					hdr = fmt.Appendf(nil, dg, name, sm.subj, sm.seq, ts.Format(time.RFC3339Nano))
@@ -5006,6 +5017,10 @@ func (mset *stream) processJetStreamMsg(subject, reply string, hdr, msg []byte, 
 	if mset.closed.Load() {
 		return errStreamClosed
 	}
+
+	// Hold isolation lock while storing message, and potentially purging as part of a rollup.
+	mset.isolateMu.Lock()
+	defer mset.isolateMu.Unlock()
 
 	mset.mu.Lock()
 	s, store := mset.srv, mset.store

hanzoai/pubsub

server/jetstream_api.go
@@ -3633,6 +3633,10 @@ func (s *Server) jsMsgGetRequest(sub *subscription, c *client, _ *Account, subje
 	var svp StoreMsg
 	var sm *StoreMsg
 
+	// Ensure this read request is isolated and doesn't interleave with writes.
+	mset.mu.RLock()
+	defer mset.mu.RUnlock()
+
 	// If AsOfTime is set, perform this first to get the sequence.
 	var seq uint64
 	if req.StartTime != nil {

server/jetstream_batching_test.go
@@ -23,6 +23,7 @@ import (
 	"math/big"
 	"strconv"
 	"strings"
+	"sync"
 	"testing"
 	"time"
 
@@ -1854,4 +1855,135 @@ func TestJetStreamAtomicBatchPublishContinuousBatchesStillMoveAppliedUp(t *testi
 	checkFor(t, 2*time.Second, 200*time.Millisecond, func() error {
 		return checkState(t, c, globalAccountName, "TEST")
 	})
+}
+
+func TestJetStreamRollupIsolatedRead(t *testing.T) {
+	const (
+		DirectGet = iota
+		DirectBatchGet
+		DirectMultiGet
+		Consumer
+	)
+
+	test := func(t *testing.T, mode int) {
+		c := createJetStreamClusterExplicit(t, "R3S", 3)
+		defer c.shutdown()
+
+		nc, js := jsClientConnect(t, c.randomServer())
+		defer nc.Close()
+
+		_, err := js.AddStream(&nats.StreamConfig{
+			Name:        "TEST",
+			Subjects:    []string{"foo.*"},
+			AllowRollup: true,
+			Replicas:    3,
+		})
+		require_NoError(t, err)
+
+		rs := c.randomNonStreamLeader(globalAccountName, "TEST")
+		mset, err := rs.globalAccount().lookupStream("TEST")
+		require_NoError(t, err)
+
+		// Reconnect to the selected server.
+		nc.Close()
+		nc, js = jsClientConnect(t, rs)
+		defer nc.Close()
+
+		m := nats.NewMsg("foo.0")
+		pubAck, err := js.PublishMsg(m)
+		require_NoError(t, err)
+		require_Equal(t, pubAck.Sequence, 1)
+
+		// mset.processJetStreamMsg will first store the new message, then update dedupe state, and then do the rollup.
+		// We block the replica such that it can store the new message but can't do the rollup yet.
+		// A read should wait for the rollup to complete.
+		mset.ddMu.Lock()
+		m.Subject = "foo.1"
+		m.Header.Set(JSMsgRollup, JSMsgRollupAll)
+		m.Header.Set(JSMsgId, "msgId")
+		_, _ = js.PublishMsg(m)
+		err = checkForErr(2*time.Second, 200*time.Millisecond, func() error {
+			var state StreamState
+			mset.store.FastState(&state)
+			if state.LastSeq != 2 {
+				return fmt.Errorf("expected last seq 2, got: %d", state.LastSeq)
+			}
+			return nil
+		})
+		if err != nil {
+			mset.ddMu.Unlock()
+			require_NoError(t, err)
+		}
+
+		// We're now subscribing and going to do a request, while the write is still halfway.
+		sub, err := nc.SubscribeSync("reply")
+		if err != nil {
+			mset.ddMu.Unlock()
+			require_NoError(t, err)
+		}
+		defer sub.Drain()
+		if err = nc.Flush(); err != nil {
+			mset.ddMu.Unlock()
+			require_NoError(t, err)
+		}
+
+		// Run two goroutines, one will unblock the first write after a short sleep,
+		// the second will do the read/consumer request.
+		var (
+			ready   sync.WaitGroup
+			run     sync.WaitGroup
+			cleanup sync.WaitGroup
+		)
+		ready.Add(2)
+		run.Add(1)
+		cleanup.Add(1)
+		go func() {
+			ready.Done()
+			defer cleanup.Done()
+			run.Wait()
+			time.Sleep(250 * time.Millisecond)
+			mset.ddMu.Unlock()
+		}()
+		go func() {
+			ready.Done()
+			run.Wait()
+			switch mode {
+			case DirectGet:
+				mset.getDirectRequest(&JSApiMsgGetRequest{LastFor: "foo.0"}, "reply")
+			case DirectBatchGet:
+				mset.getDirectRequest(&JSApiMsgGetRequest{NextFor: "foo.*", Batch: 2}, "reply")
+			case DirectMultiGet:
+				mset.getDirectRequest(&JSApiMsgGetRequest{MultiLastFor: []string{"foo.*"}, Batch: 2}, "reply")
+			case Consumer:
+				mset.addConsumer(&ConsumerConfig{DeliverSubject: "reply", Direct: true})
+			}
+		}()
+
+		// Wait for both to be ready, then run both of them.
+		ready.Wait()
+		run.Done()
+
+		msg, err := sub.NextMsg(time.Second)
+		// Make sure cleanup has happened before validating.
+		cleanup.Wait()
+		require_NoError(t, err)
+		if mode == DirectGet {
+			require_Equal(t, msg.Header.Get("Status"), "404")
+		} else if mode != Consumer {
+			require_Equal(t, msg.Header.Get(JSNumPending), "0")
+			require_Equal(t, msg.Header.Get(JSSequence), "2")
+		} else {
+			// $JS.ACK.<stream>.<consumer>.<delivered>.<sseq>.<cseq>.<tm>.<pending>
+			require_True(t, strings.HasPrefix(msg.Reply, jsAckPre))
+			tokens := strings.Split(msg.Reply, ".")
+			require_Len(t, len(tokens), 9)
+			require_Equal(t, tokens[8], "0") // pending
+			require_Equal(t, tokens[5], "2") // sseq
+		}
+	}
+
+	t.Run("DirectGet", func(t *testing.T) { test(t, DirectGet) })
+	t.Run("DirectBatchGet", func(t *testing.T) { test(t, DirectBatchGet) })
+	t.Run("DirectMultiGet", func(t *testing.T) { test(t, DirectMultiGet) })
+	t.Run("Consumer", func(t *testing.T) { test(t, Consumer) })
 }

server/stream.go
@@ -4956,9 +4956,10 @@ func (mset *stream) getDirectMulti(req *JSApiMsgGetRequest, reply string) {
 	// TODO(dlc) - Make configurable?
 	const maxAllowedResponses = 1024
 
-	// We hold the lock here to try to avoid changes out from underneath of us.
+	// Ensure this read request is isolated and doesn't interleave with writes.
 	mset.mu.RLock()
 	defer mset.mu.RUnlock()
+
 	// Grab store and name.
 	store, name, s := mset.store, mset.cfg.Name, mset.srv
 
@@ -5008,7 +5009,7 @@ func (mset *stream) getDirectMulti(req *JSApiMsgGetRequest, reply string) {
 		return
 	}
 
-	np, lseq, sentBytes, sent := uint64(len(seqs)-1), uint64(0), 0, 0
+	np, lseq, sentBytes, sent := uint64(len(seqs)), uint64(0), 0, 0
 	for _, seq := range seqs {
 		if seq < req.Seq {
 			if np > 0 {
@@ -5027,6 +5028,10 @@ func (mset *stream) getDirectMulti(req *JSApiMsgGetRequest, reply string) {
 		hdr := sm.hdr
 		ts := time.Unix(0, sm.ts).UTC()
 
+		// Decrement num pending. This is an optimization, and we do not continue to look it up for these operations.
+		if np > 0 {
+			np--
+		}
 		if len(hdr) == 0 {
 			hdr = fmt.Appendf(nil, dgb, name, sm.subj, sm.seq, ts.Format(time.RFC3339Nano), np, lseq)
 		} else {
@@ -5037,10 +5042,6 @@ func (mset *stream) getDirectMulti(req *JSApiMsgGetRequest, reply string) {
 			hdr = genHeader(hdr, JSTimeStamp, ts.Format(time.RFC3339Nano))
 			hdr = genHeader(hdr, JSNumPending, strconv.FormatUint(np, 10))
 			hdr = genHeader(hdr, JSLastSequence, strconv.FormatUint(lseq, 10))
-		}
-		// Decrement num pending. This is optimization and we do not continue to look it up for these operations.
-		if np > 0 {
-			np--
 		}
 		// Track our lseq
 		lseq = sm.seq
@@ -5071,9 +5072,11 @@ func (mset *stream) getDirectRequest(req *JSApiMsgGetRequest, reply string) {
 		return
 	}
 
+	// Ensure this read request is isolated and doesn't interleave with writes.
 	mset.mu.RLock()
+	defer mset.mu.RUnlock()
+
 	store, name, s := mset.store, mset.cfg.Name, mset.srv
-	mset.mu.RUnlock()
 
 	var seq uint64
 	// Lookup start seq if AsOfTime is set.
@@ -5144,6 +5147,10 @@ func (mset *stream) getDirectRequest(req *JSApiMsgGetRequest, reply string) {
 		if !req.NoHeaders {
 			hdr = sm.hdr
 			if isBatchRequest {
+				// Decrement num pending. This is an optimization, and we do not continue to look it up for these operations.
+				if np > 0 {
+					np--
+				}
 				if len(hdr) == 0 {
 					hdr = fmt.Appendf(nil, dgb, name, sm.subj, sm.seq, ts.Format(time.RFC3339Nano), np, lseq)
 				} else {
@@ -5155,8 +5162,6 @@ func (mset *stream) getDirectRequest(req *JSApiMsgGetRequest, reply string) {
 					hdr = genHeader(hdr, JSNumPending, strconv.FormatUint(np, 10))
 					hdr = genHeader(hdr, JSLastSequence, strconv.FormatUint(lseq, 10))
 				}
-				// Decrement num pending. This is optimization and we do not continue to look it up for these operations.
-				np--
 			} else {
 				if len(hdr) == 0 {
 					hdr = fmt.Appendf(nil, dg, name, sm.subj, sm.seq, ts.Format(time.RFC3339Nano))
@@ -5232,6 +5237,8 @@ func (mset *stream) processJetStreamMsg(subject, reply string, hdr, msg []byte, 
 	if mset.closed.Load() {
 		return errStreamClosed
 	}
+
+	// Hold isolation lock while storing message, and potentially purging as part of a rollup.
 
 	mset.mu.Lock()
 	s, store := mset.srv, mset.store